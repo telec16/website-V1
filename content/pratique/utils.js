cfabf70ac0ae6b3cb3e64693167587f5b54b7418
telec <>
2020-08-07 17:30:45 +0200

    Disclaimer

--- content/pratique/utils.js
+++ content/pratique/utils.js
@@ -11,13 +11,13 @@ function getChildByName(div, childName)
 function getChildsByTagName(div, childTagName)
 {
 	var childs=new Array();
-	
+
 	for(var i = 0; i<div.childNodes.length; i++)
 	{
 		if(div.childNodes[i].tagName == childTagName)
 			childs.push(div.childNodes[i]);
 	}
-	
+
 	return childs;
 }
 
@@ -25,28 +25,28 @@ function fillWithSpaces(inStr, space)
 {
 	var outStr = inStr;
 	var space = (space-inStr.length);
-	
+
 	for(var i=0 ; i<space; i++)
 		outStr += " ";
-	
+
 	return outStr;
 }
 
 function searchForValue(array, value)
 {
-	for (var key in array) 
+	for (var key in array)
 	{
 		if (key === 'length' || !array.hasOwnProperty(key)) continue;
-		
+
 		var innerArray = array[key];
-		
-		for (var i=0; i<innerArray.length; i++) 
+
+		for (var i=0; i<innerArray.length; i++)
 		{
 			if(innerArray[i] == value)
-				return key;			
+				return key;
 		}
 	}
-	
+
 	return null;
 }
 
@@ -74,11 +74,11 @@ function getMousePos(canvas, evt) {
 
 function setDragListener(cnv, callbck){
 	var isDown=false;
-	
+
 	cnv.onmousedown = mouseDown;
 	cnv.onmousemove = mouseMove;
 	document.onmouseup = mouseUp;
-	
+
 	function mouseDown(e){
 		isDown=true;
 		mouseMove(e);
@@ -86,12 +86,12 @@ function setDragListener(cnv, callbck){
 	function mouseUp(e){
 		isDown=false;
 	}
-	
+
 	function mouseMove(e){
 		if(isDown){
 			e = e || window.event;
 			e.preventDefault();
-			
+
 			var pos = getMousePos(cnv, e);
 			callbck(pos);
 		}
@@ -113,7 +113,7 @@ function map(val, fMin, fMax, tMin, tMax){
 	return ((val-fMin)/(fMax-fMin))*(tMax-tMin)+tMin; //Map
 }
 
-			
+
 function closestKeyInList(list, key){
 	return closestInListAbs(Object.keys(list), key);
 }
@@ -121,38 +121,38 @@ function closestKeyInList(list, key){
 function closestInListAbs(list, value){
 	var minError=Math.abs((value-list[0])/list[0]);
 	var c=parseFloat(list[0]);
-	
+
 	for(let l of list){
 		l = parseFloat(l)
 		var e = Math.abs((value-l)/l);
-		
+
 		if(e < minError){
 			minError = e;
 			c = l;
 		}
 	}
-	
+
 	return c;
 }
 
 function closestInList(list, value){
 	var v = parseFloat((value).toExponential().split("e")[0]);
 	var p = parseFloat((value).toExponential().split("e")[1]);
-	
+
 	var minError=1;
 	var c=0;
-	
+
 	for(let l of list){
 		l = parseFloat(l)
 		l = parseFloat((l).toExponential().split("e")[0]);
 		var e = Math.abs((v-l)/l);
-		
+
 		if(e < minError){
 			minError = e;
 			c = l;
 		}
 	}
-	
+
 	return c*Math.pow(10, p);
 }
 
@@ -163,7 +163,7 @@ function getResistorSerie(s){
 		 "48": [100, 105, 110, 115, 121, 127, 133, 140, 147, 154, 162, 169, 178, 187, 196, 205, 215, 226, 237, 249, 261, 274, 287, 301, 316, 332, 348, 365, 383, 402, 422, 442, 464, 487, 511, 536, 562, 590, 619, 649, 681, 715, 750, 787, 825, 866, 909, 953],
 		 "96": [100, 102, 105, 107, 110, 113, 115, 118, 121, 124, 127, 130, 133, 137, 140, 143, 147, 150, 154, 158, 162, 165, 169, 174, 178, 182, 187, 191, 196, 200, 205, 210, 215, 221, 226, 232, 237, 243, 249, 255, 261, 267, 274, 280, 287, 294, 301, 309, 316, 324, 332, 340, 348, 357, 365, 374, 383, 392, 402, 412, 422, 432, 442, 453, 464, 475, 487, 499, 511, 523, 536, 549, 562, 576, 590, 604, 619, 634, 649, 665, 681, 698, 715, 732, 750, 768, 787, 806, 825, 845, 866, 887, 909, 931, 953, 976],
 		 "192":[100, 101, 102, 104, 105, 106, 107, 109, 110, 111, 113, 114, 115, 117, 118, 120, 121, 123, 124, 126, 127, 129, 130, 132, 133, 135, 137, 138, 140, 142, 143, 145, 147, 149, 150, 152, 154, 156, 158, 160, 162, 164, 165, 167, 169, 172, 174, 176, 178, 180, 182, 184, 187, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213, 215, 218, 221, 223, 226, 229, 232, 234, 237, 240, 243, 246, 249, 252, 255, 258, 261, 264, 267, 271, 274, 277, 280, 284, 287, 291, 294, 298, 301, 305, 309, 312, 316, 320, 324, 328, 332, 336, 340, 344, 348, 352, 357, 361, 365, 370, 374, 379, 383, 388, 392, 397, 402, 407, 412, 417, 422, 427, 432, 437, 442, 448, 453, 459, 464, 470, 475, 481, 487, 493, 499, 505, 511, 517, 523, 530, 536, 542, 549, 556, 562, 569, 576, 583, 590, 597, 604, 612, 619, 626, 634, 642, 649, 657, 665, 673, 681, 690, 698, 706, 715, 723, 732, 741, 750, 759, 768, 777, 787, 796, 806, 816, 825, 835, 845, 856, 866, 876, 887, 898, 909, 920, 931, 942, 953, 965, 976, 988]};
-		
+
 	return E[s];
 }
 
